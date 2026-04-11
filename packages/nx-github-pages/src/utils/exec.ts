566cf289d44b29ab9a502163d95a2b57c2808f32
Craigory Coppola <craigorycoppola@gmail.com>
2026-04-11 12:21:23 -0400

    fix(core): use spawn instead of exec to avoid maxBuffer limitations

--- packages/nx-github-pages/src/utils/exec.ts
+++ packages/nx-github-pages/src/utils/exec.ts
@@ -1,26 +1,38 @@
-import { exec as execCallback, ExecOptions } from 'child_process';
+import { spawn, SpawnOptions } from 'child_process';
 
 export async function exec(
   command: string,
-  options: ExecOptions & { stdio?: 'inherit' | 'ignore' }
+  options: Omit<SpawnOptions, 'shell' | 'stdio'> & {
+    stdio?: 'inherit' | 'ignore';
+  }
 ) {
   const { stdio, ...childProcessArgs } = options;
   return new Promise<string>((resolve, reject) => {
-    const childProcess = execCallback(
-      command,
-      childProcessArgs,
-      (error, stdout) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(stdout);
-        }
-      }
-    );
+    const childProcess = spawn(command, {
+      shell: true,
+      ...childProcessArgs,
+    });
+
+    let stdout = '';
+    if (childProcess.stdout) {
+      childProcess.stdout.on('data', (data) => {
+        stdout += data;
+      });
+    }
 
     if ((stdio ?? 'inherit') === 'inherit') {
       childProcess.stdout?.pipe(process.stdout);
       childProcess.stderr?.pipe(process.stderr);
     }
+
+    childProcess.on('close', (code) => {
+      if (code !== 0) {
+        reject(
+          new Error(`Command failed with exit code ${code}: ${command}`)
+        );
+      } else {
+        resolve(stdout);
+      }
+    });
   });
 }
